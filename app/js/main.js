7e38fbfc9426ba4733afc5b39689f60795e65170
Ruslan Idrisov <rusl-idrisov@ya.ru>
2015-04-27 22:04:32 +0500

    исправил ошибки переполнения контентом, прибил футер к низу

--- app/js/main.js
+++ app/js/main.js
@@ -9,5 +9,17 @@ $('.uploat-close').click(function(event) {
 	$('.uploat').removeClass('uploat-show');
 	$('.saver').removeClass('block');
 });	
+	$(':input').blur(function() {
+if ($(this).val().length == 0) {
+$(this)
+.addClass('error');
+//.after('<span class="error">This field must ... </span>');
+}
+});
+$(':input').focus(function() {
+$(this)
+.removeClass('error')
+.next('span')
+.remove();
 	});
-
+});
